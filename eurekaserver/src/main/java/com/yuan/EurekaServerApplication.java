497bec21484d9fe999d9c9591cba8067ce3d1a03
YuanCoder <yuan_268311@163.com>
2017-06-22 16:55:21 +0800

    初始化系统

---
+++ eurekaserver/src/main/java/com/yuan/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.yuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
