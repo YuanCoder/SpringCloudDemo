03d769fbeb4c1d18f8bbd3d7b9a65d85971f60c2
YuanCoder <yuan_268311@163.com>
2017-07-03 09:53:06 +0800

    bug修复

--- servicemiya/src/main/java/com/yuan/ServiceMiyaApplication.java
+++ servicemiya/src/main/java/com/yuan/ServiceMiyaApplication.java
@@ -5,15 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class ServiceMiyaApplication {
 
 	private static final Logger LOG = Logger.getLogger(ServiceMiyaApplication.class.getName());
